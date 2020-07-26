63d6bab561176384523efaa3276655540df71609
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-07-26 21:20:17 +0900

    add: login/logout

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,6 +1,35 @@
-import {Request, Response, Router} from "express";
+import {NextFunction, Request, Response, Router} from "express";
 
 const router = Router();
+
+function requireAuth(req: Request, res: Response, next: NextFunction ): void {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
+}
+
+router.get('/', (req: Request, res: Response) =>{
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+          <div>
+            <div>Your are logged in</div>
+            <a href="/logout">Logout</a>
+          </div>
+        `);
+
+    } else {
+        res.send(`
+          <div>
+            <div>Your are not logged in</div>
+            <a href="/login">Login</a>
+          </div>
+        `);
+
+    }
+});
 
 router.get('/login', (req: Request, res: Response) => {
     res.send(`
@@ -19,7 +48,22 @@ router.get('/login', (req: Request, res: Response) => {
 
 router.post('/login', (req: Request, res: Response) => {
     const { email, password } = req.body;
-    res.send(email + password);
+    if (email && password && email === 'hi@gmail.com' && password === 'abc') {
+        req.session = { loggedIn: true };
+        res.redirect('/');
+
+    } else {
+        res.send('invalid email or password');
+    }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+    req.session = null;
+    res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send('welcome to protected page');
 });
 
 export { router };
